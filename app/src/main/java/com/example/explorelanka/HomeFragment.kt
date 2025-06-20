dd48a5f61ea9cf3d5181e7e4416a291e3babf0d5
Thisara Shalitha <thisarashalitha100@gmail.com>
2025-06-20 21:22:13 +0530

    dashboard create

---
+++ app/src/main/java/com/example/explorelanka/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.explorelanka
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.widget.EditText
+
+class HomeFragment : Fragment() {
+
+    private lateinit var jobRecyclerView: RecyclerView
+
+    private lateinit var searchEditText: EditText
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+    }
+
