2f7010c9327fc315a32ca72d8c231f134234a83b
Thisara Shalitha <thisarashalitha100@gmail.com>
2025-07-08 00:41:58 +0530

    Searchbar implement

--- app/src/main/java/com/example/explorelanka/ViewActivity.kt
+++ app/src/main/java/com/example/explorelanka/ViewActivity.kt
@@ -1,11 +1,19 @@
 package com.example.explorelanka
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class ViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view) // Make sure this file exists in res/layout
+
+
+        // Back button setup
+        val backButton: ImageButton = findViewById(R.id.btnBack1)
+        backButton.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
     }
 }
